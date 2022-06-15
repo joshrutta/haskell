b1a40df1afd0b9d476d399d5ca586f08d30506fa
joshrutta <joshrutta7@gmail.com>
2022-06-14 23:16:41 -0400

    wrote caesar and unCaesar functoins

--- caesar.hs
+++ caesar.hs
@@ -1,2 +1,12 @@
-module CaesarCipher where
+module Cipher where
 
+import Data.Char
+
+shiftLetter :: Int -> Char -> Char
+shiftLetter shift letter = chr ((ord letter + shift - 97) `mod` 26 + 97)
+
+caesar :: String -> Int -> String
+caesar xs key = map (shiftLetter key) xs
+
+unCaesar :: [Char] -> Int -> [Char]
+unCaesar xs key = map (shiftLetter (- key)) xs
