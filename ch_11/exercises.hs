2c7c91ee385beb516e22df87b91ed5f22ed621b2
Josh Rutta <jrutta@salesforce.com>
2022-07-06 09:19:19 -0400

    adding more to exercises in ch 11 for completeness

--- ch_11/exercises.hs
+++ ch_11/exercises.hs
@@ -1,6 +1,27 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving, FlexibleInstances #-}
+
 module Exercises where
 
 import Data.Int
+
+-- LogicGoats
+
+class TooMany a where
+    tooMany :: a -> Bool 
+
+instance TooMany Int where
+    tooMany n = n > 42
+
+instance TooMany (Int, String) where
+    tooMany (n, _) = tooMany n
+
+instance TooMany (Int, Int) where
+    tooMany (a, b) = tooMany (a + b)
+
+instance (TooMany a, Num a) => TooMany (a,a) where
+    tooMany (a, b) = tooMany (a + b)
+
+newtype Goats = Goats Int deriving (Eq, Show, TooMany)
 
 -- Pity The Bool
 
